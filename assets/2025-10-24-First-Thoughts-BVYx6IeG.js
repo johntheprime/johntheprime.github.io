6382d448078cb9ace059b6c17cd3be9e9c61415f
johntheprime <johntheprime@users.noreply.github.com>
2026-03-19 13:05:45 +0000

    Deploy: 2a84fe5cd31918d3959417ecbc394d0bcdaa352e 2a84fe5cd31918d3959417ecbc394d0bcdaa352e

---
+++ assets/2025-10-24-First-Thoughts-BVYx6IeG.js
@@ -0,0 +1,2 @@
+var e=`Chill threads the air, yet my pulse drifts in the molten haze of a sun that no frost can touch.
+`;export{e as default};
---
+++ assets/2025-10-27-Markdown-Resume-CUMd7_we.js
@@ -0,0 +1,2 @@
+var e=`This Markdown resume generator supports custom CSS styling and PDF export. Good for English resumes.
+- [Markdown Resume github repo](https://github.com/junian/markdown-resume) `;export{e as default};
